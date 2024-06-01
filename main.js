bd5b8986bc58b8cb688bb9b2c4c1f17147954ad6
zerinoid <leo.zerino@gmail.com>
2024-05-31 22:08:30 -0300

    fix: clean up

--- main.js
+++ main.js
@@ -1,30 +1,14 @@
 import "./style.css";
 
-import viteLogo from "/vite.svg";
-
 import { setupCounter } from "./counter.js";
 import fetchData from "./lib/fetchData";
-import javascriptLogo from "./javascript.svg";
 
-const broths = await fetchData("broths");
-const proteins = await fetchData("proteins");
+// const broths = await fetchData("broths");
+// const proteins = await fetchData("proteins");
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
+// document.querySelector("#app").innerHTML = `
+//   <div>
+//   </div>
+// `;
 
 setupCounter(document.querySelector("#counter"));
