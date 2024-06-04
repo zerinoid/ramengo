c2cb8612116f3fb74e5c1d216ad45c99f90de872
zerinoid <leo.zerino@gmail.com>
2024-06-04 05:36:31 -0300

    feat: order submission logic

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import './src/styles/main.scss'
 import { fetchIngredients } from './src/ingredients.js'
+import submitData from './lib/submitData'
 
 window.addEventListener('onChangeIngredient', event => {
   reqObject[event.detail[0]] = event.detail[1]
@@ -10,10 +11,16 @@ const reqObject = {
   proteinId: '0'
 }
 
-// PARA TESTE
+const handleSubmit = async data => {
+  const res = await submitData(data)
+  if (res.ok) {
+    console.log(res.json(), '### res  ###')
+  }
+}
+
 document
   .querySelector('#submit')
-  .addEventListener('click', () => console.log(reqObject, '### reqObject  ###'))
+  .addEventListener('click', () => handleSubmit(reqObject))
 
 fetchIngredients('broths')
 fetchIngredients('proteins')
